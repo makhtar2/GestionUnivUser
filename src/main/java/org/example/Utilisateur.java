2e175d1156548c3732ef70b156d2421ccc4c1ac8
Makhtar <makhtar2gsm@gmail.com>
2026-05-02 15:26:00 +0000

    Creating User Class

---
+++ src/main/java/org/example/Utilisateur.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Utilisateur {
+    String Nom;
+    String Prenom;
+    String Role;
+    int Telephone;
+    int Email;
+
+    public Utilisateur( String Nom, String Prenom, String Role, int Telephone, int Email){
+        this.Nom = Nom;
+        this.Prenom=Prenom;
+        this.Role=Role;
+        this.Email=Email;
+        this.Telephone=Telephone;
+    }
+
+
+}
